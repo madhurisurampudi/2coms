16beff9389144f897c0e36fc9ab8c61c682d0541
Ananta Munuganti <Ananta Munuganti@DESKTOP-FCOL8H7>
2022-06-10 20:05:08 +0530

    commit 1: welcome message

---
+++ NewAppGitHub/src/com/twocoms/app/App_01.java
@@ -0,0 +1,13 @@
+package com.twocoms.app;
+
+public class App_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.print("Welcome to GitHub");
+		
+	}
+
+}
